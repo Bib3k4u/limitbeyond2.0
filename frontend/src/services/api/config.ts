61a08337be51a41ce96660f632a675f8d2e723b0
Govind Ray <govindray@Govinds-MacBook-Air.local>
2026-03-29 18:10:32 +0530

    cleaned code for limitbeyond

--- frontend/src/services/api/config.ts
+++ frontend/src/services/api/config.ts
@@ -1,4 +1,4 @@
-// const API_BASE_URL = 'https://limitbeyond2-0.onrender.com/api';
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://limitbeyond-backend-latest.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:8080/api';
 
 export default API_BASE_URL;
